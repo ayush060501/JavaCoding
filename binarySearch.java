3bf05baf3f5b3b6b18246fbd54fab3107d9d7628
gulatikeshav <133638164+gulatikeshav@users.noreply.github.com>
2023-10-06 18:03:30 +0530

    Create  binarySearch.java
    Please review my code and merge it .

---
+++ binarySearch.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class binarySearch {
+    
+    public static int binarySearch(int numbers[],int key){
+        int start = 0, end = numbers.length -1;
+
+        while(start <= end){
+            int mid = (start + end) / 2;
+          
+            if(numbers[mid] == key){  // element found
+                return mid;
+            }
+            if(numbers[mid] < key){  
+                start = mid + 1;
+            } else {  
+                end = mid - 1;
+            }
+        }
+        return -1;
+
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        // prerequisite -> sorted arrays
+
+        int numbers[] = {2,4,6,8,10,12,14};
+        int key = 34;
+
+        System.out.println("Index for key is : " + binarySearch(numbers, key));
+
+        sc.close();
+    }
+}
